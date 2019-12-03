a4260c98721783c8d0dcd41ecdfc66517c6ccad9
Vishal <neharkar.vishal@gmail.com>
2019-12-03 20:48:51 +0530

    Removed unused `import`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
