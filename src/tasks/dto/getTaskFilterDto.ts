e852a8b03b3dcf7482e682964c1c9581688a934a
Vishal <neharkar.vishal@gmail.com>
2019-12-03 19:18:59 +0530

    ValidationPipe Task Filtering and Search

--- src/tasks/dto/getTaskFilterDto.ts
+++ src/tasks/dto/getTaskFilterDto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '../task.model';
+import { IsEmpty, IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.OPEN, TaskStatus.IN_PROGRESS])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
