4812cc8e7f5995412c29ba59c63d805e3817fd2a
Vishal <neharkar.vishal@gmail.com>
2019-12-18 19:21:02 +0530

    Setiing up Auth module

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Task } from '../tasks/task.entity';
+import { User } from '../auth/user.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -9,6 +10,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: 'root', // TODO: use `.env` for sensitive data
   database: 'taskmanagement',
   // entities: [`${__dirname}/../**/*.entity.ts`],
-  entities: [Task],
+  entities: [Task, User],
   synchronize: false, // setting `true` not recommended in production
 };
