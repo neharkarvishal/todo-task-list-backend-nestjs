d0e9ce9807f42fbb7780590593e0c483e599b7cf
Vishal <neharkar.vishal@gmail.com>
2019-12-18 20:20:00 +0530

    Setting up AuthModule, User Entity and UserRepository for Sign up

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -9,22 +9,21 @@ export class TaskRepository extends Repository<Task> {
   async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
-
     if (status) {
       query.andWhere('task.status = :status', { status });
     }
-
     if (search) {
       query.andWhere(
         '(task.title LIKE :search OR task.description LIKE :search)',
         { search: `%${search}%` },
         );
     }
+
     return await query.getMany();
   }
+
   async creatTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description } = createTaskDto;
-
     const task = new Task();
     task.title = title;
     task.description = description;
