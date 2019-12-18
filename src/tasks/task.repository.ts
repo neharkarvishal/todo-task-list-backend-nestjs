b56bbd7699fc1c763093aa0d3813c89a7972adc7
Vishal <neharkar.vishal@gmail.com>
2019-12-18 18:38:18 +0530

    Persistence Getting Tasks (with filters)

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -10,6 +10,16 @@ export class TaskRepository extends Repository<Task> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
 
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+        );
+    }
     return await query.getMany();
   }
   async creatTask(createTaskDto: CreateTaskDto): Promise<Task> {
